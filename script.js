0ef5f4679d77f708da5a9730f6fbe23c72c002bd
saitejx <saitejatelukutla@gmail.com>
2026-05-18 21:11:30 +0530

    developed subscription components UI and added 5 components

--- script.js
+++ script.js
@@ -307,6 +307,10 @@ function handleSearch(event) {
     footer:  "footer.html",
     color:   "color.html",
     colors:  "color.html",
+    pricing: "pricing.html",
+    subscription: "subscription.html",
+    subscriptions: "subscription.html",
+    billing: "subscription.html",
   };
 
   for (const key in routes) {
